30701acebdf4aad5f324b48355ab043b092c7be6
Rongkun Pu <cangzhiyue9@gmail.com>
2026-03-18 20:12:55 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      // 前端使用 /api/* 访问后端，开发时由 Vite 转发到 Express
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
+  }
+})
