dc4a8e1b5376ec9698cbe26593180f558c40fc92
yuecangzhi <cangzhiyue9@gmail.com>
2026-03-19 16:47:14 +0800

    回退
    回退测试

--- server/index.js
+++ server/index.js
@@ -13,10 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: 'https://piperas-blog.edgeone.dev/', // 替换为你的 EdgeOne 域名
-  credentials: true
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
